c2bd625c6c61e6fe0b6b35d79fbe38800ed897f5
Toaster <citycodetoaster@gmail.com>
2025-04-18 01:31:24 +0800

    Stop tracking cmake-build-debug/

--- cmake-build-debug/_deps/sdl2_mixer-build/CMakeFiles/SDL2_mixer.dir/src/codecs/music_cmd.c.obj.d
+++
@@ -1,6 +0,0 @@
-_deps/sdl2_mixer-build/CMakeFiles/SDL2_mixer.dir/src/codecs/music_cmd.c.obj: \
- D:\Users\User\Desktop\113-2_oop\bad-ice-cream\PTSD\lib\sdl2_mixer\src\codecs\music_cmd.c \
- D:/Users/User/Desktop/113-2_oop/bad-ice-cream/cmake-build-debug/_deps/sdl2-build/include-config-debug/SDL_config.h \
- D:/Users/User/Desktop/113-2_oop/bad-ice-cream/cmake-build-debug/_deps/sdl2-build/include/SDL_platform.h \
- D:/Users/User/Desktop/113-2_oop/bad-ice-cream/cmake-build-debug/_deps/sdl2-build/include/begin_code.h \
- D:/Users/User/Desktop/113-2_oop/bad-ice-cream/cmake-build-debug/_deps/sdl2-build/include/close_code.h
--- cmake-build-debug/_deps/sdl2_mixer-build/CMakeFiles/SDL2_mixer.dir/src/codecs/music_opus.c.obj.d
+++
@@ -1,2 +0,0 @@
-_deps/sdl2_mixer-build/CMakeFiles/SDL2_mixer.dir/src/codecs/music_opus.c.obj: \
- D:\Users\User\Desktop\113-2_oop\bad-ice-cream\PTSD\lib\sdl2_mixer\src\codecs\music_opus.c
